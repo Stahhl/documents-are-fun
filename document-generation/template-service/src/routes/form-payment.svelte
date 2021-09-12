0f74b456fe8d3580277d7781683550cbaff8db8f
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-09-12 19:25:36 +0200

    templte-service sveltekit

---
+++ document-generation/template-service/src/routes/form-payment.svelte
@@ -0,0 +1,14 @@
+<script>
+    let check = false;
+    let text = "";
+</script>
+
+<h1>Payment form</h1>
+
+<form class="content">
+    <input type=checkbox bind:checked={check}>
+
+    {#if check}
+        <input type="text" bind:value={text}>
+    {/if}
+</form>
