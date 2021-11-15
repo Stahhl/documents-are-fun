d95658f9a918c868e7b41468d24bda8b8759c731
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2021-11-15 22:57:03 +0100

    ejs start

--- src/generator-service-express/index.ts
+++ src/generator-service-express/index.ts
@@ -3,8 +3,11 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => res.send("Express + TypeScript Server"));
+app.set("view engine", "ejs");
+app.set("views", "./templates");
+
+app.get("/:id", (req, res) => res.render(`${req.params.id}/${req.params.id}-wizard`, {person:{name: "John", age: "25"}}));
 
 app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
+  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
---
+++ src/generator-service-express/templates/order/order-data.ts
@@ -0,0 +1,7 @@
+import { Product } from "../../models/Product";
+import { Person } from "../../models/Person";
+
+export interface OrderData{
+    customer: Person;
+    //products: Product[];
+}
